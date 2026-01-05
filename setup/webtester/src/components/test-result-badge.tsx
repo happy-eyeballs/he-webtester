26988a9bf33695ed0bffedcca7eef0e08fffb18c
Matthias Kirstein <kirm@net.in.tum.de>
2026-01-05 16:52:04 +0100

    Add clickable tooltip

--- setup/webtester/src/components/test-result-badge.tsx
+++ setup/webtester/src/components/test-result-badge.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { type SubtestResult } from "@/lib/test-run.ts";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip.tsx";
+import { ClickableTooltip } from "@/components/clickable-tooltip.tsx";
 
 type Props = {
   result: SubtestResult;
@@ -13,8 +9,8 @@ type Props = {
 
 export const TestResultBadge: React.FC<Props> = ({ result, metadata }) => {
   return (
-    <Tooltip>
-      <TooltipTrigger asChild>
+    <ClickableTooltip
+      trigger={
         <div
           style={{
             background: result.color,
@@ -23,66 +19,69 @@ export const TestResultBadge: React.FC<Props> = ({ result, metadata }) => {
         >
           {result.value}
         </div>
-      </TooltipTrigger>
-      <TooltipContent>
-        <div className="grid grid-cols-[auto_1fr] gap-x-4 gap-y-1.5 p-2">
-          <div className="text-muted text-right font-semibold">URL</div>
-          <a
-            href={result.url}
-            className="underline underline-offset-4 cursor-pointer hover:opacity-80"
-          >
-            {result.url}
-          </a>
+      }
+    >
+      <ResultBadgeTooltipContent result={result} metadata={metadata ?? []} />
+    </ClickableTooltip>
+  );
+};
 
-          {metadata?.map(({ key, value }) => (
-            <>
-              <div className="text-muted text-right font-semibold" key={key}>
-                {key}
-              </div>
-              <div>{value}</div>
-            </>
-          ))}
+const ResultBadgeTooltipContent: React.FC<Props> = ({ result, metadata }) => {
+  return (
+    <div className="grid grid-cols-[auto_1fr] gap-x-4 gap-y-1.5 p-2">
+      <div className="text-muted text-right font-semibold">URL</div>
+      <a
+        href={result.url}
+        className="underline underline-offset-4 cursor-pointer hover:opacity-80"
+      >
+        {result.url}
+      </a>
 
-          {result.error && (
-            <>
-              <div className="text-muted text-right font-semibold">Error</div>
-              <div>{result.error}</div>
-            </>
-          )}
+      {metadata?.map(({ key, value }) => (
+        <>
+          <div className="text-muted text-right font-semibold" key={key}>
+            {key}
+          </div>
+          <div>{value}</div>
+        </>
+      ))}
 
-          {result.requestTiming && (
-            <>
-              <div className="text-muted text-right font-semibold">
-                Total Fetch Duration
-              </div>
-              <div>{result.requestTiming.totalDurationMs.toFixed(2)} ms</div>
+      {result.error && (
+        <>
+          <div className="text-muted text-right font-semibold">Error</div>
+          <div>{result.error}</div>
+        </>
+      )}
 
-              <div className="text-muted text-right font-semibold">
-                DNS Lookup Duration
-              </div>
-              <div>
-                {result.requestTiming.dnsLookupDurationMs.toFixed(2)} ms
-              </div>
+      {result.requestTiming && (
+        <>
+          <div className="text-muted text-right font-semibold">
+            Total Fetch Duration
+          </div>
+          <div>{result.requestTiming.totalDurationMs.toFixed(2)} ms</div>
 
-              <div className="text-muted text-right font-semibold">
-                Connect Duration
-              </div>
-              <div>
-                {`${result.requestTiming.connectionEstablishmentDurationMs.toFixed(
-                  2,
-                )} ms (TLS negotiation: ${result.requestTiming.tlsNegotiationDurationMs.toFixed(
-                  2,
-                )} ms)`}
-              </div>
+          <div className="text-muted text-right font-semibold">
+            DNS Lookup Duration
+          </div>
+          <div>{result.requestTiming.dnsLookupDurationMs.toFixed(2)} ms</div>
 
-              <div className="text-muted text-right font-semibold">
-                Request Duration
-              </div>
-              <div>{result.requestTiming.requestDurationMs.toFixed(2)} ms</div>
-            </>
-          )}
-        </div>
-      </TooltipContent>
-    </Tooltip>
+          <div className="text-muted text-right font-semibold">
+            Connect Duration
+          </div>
+          <div>
+            {`${result.requestTiming.connectionEstablishmentDurationMs.toFixed(
+              2,
+            )} ms (TLS negotiation: ${result.requestTiming.tlsNegotiationDurationMs.toFixed(
+              2,
+            )} ms)`}
+          </div>
+
+          <div className="text-muted text-right font-semibold">
+            Request Duration
+          </div>
+          <div>{result.requestTiming.requestDurationMs.toFixed(2)} ms</div>
+        </>
+      )}
+    </div>
   );
 };
