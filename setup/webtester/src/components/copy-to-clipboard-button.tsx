e938e6ba9a87389529f83c30e5693126a5828d4e
Matthias Kirstein <kirm@net.in.tum.de>
2026-01-05 14:46:56 +0100

    Improve citation dialog

--- setup/webtester/src/components/copy-to-clipboard-button.tsx
+++ setup/webtester/src/components/copy-to-clipboard-button.tsx
@@ -8,7 +8,10 @@ import {
 } from "@/components/ui/tooltip.tsx";
 import { cn } from "@/lib/utils.ts";
 
-export const CopyToClipboardButton: React.FC<{ text: string }> = ({ text }) => {
+export const CopyToClipboardButton: React.FC<{
+  text: string;
+  children?: React.ReactNode;
+}> = ({ text, children }) => {
   const [hasCopied, setHasCopied] = useState(false);
 
   const copyToClipboard = async () => {
@@ -21,20 +24,24 @@ export const CopyToClipboardButton: React.FC<{ text: string }> = ({ text }) => {
   return (
     <Tooltip>
       <TooltipTrigger asChild={true}>
-        <Button size="icon" className="relative" onClick={copyToClipboard}>
-          <CopyIcon
-            className={cn(
-              "absolute h-4 w-4 transition-all duration-300",
-              hasCopied ? "scale-0 opacity-0" : "scale-100 opacity-100",
-            )}
-          />
+        <Button onClick={copyToClipboard} size={children ? undefined : "icon"}>
+          <div className="relative flex items-center justify-center size-4">
+            <CopyIcon
+              className={cn(
+                "absolute size-4 transition-all duration-300",
+                hasCopied ? "scale-0 opacity-0" : "scale-100 opacity-100",
+              )}
+            />
 
-          <CheckIcon
-            className={cn(
-              "absolute h-4 w-4 transition-all duration-300",
-              hasCopied ? "scale-100 opacity-100" : "scale-0 opacity-0",
-            )}
-          />
+            <CheckIcon
+              className={cn(
+                "absolute size-4 transition-all duration-300",
+                hasCopied ? "scale-100 opacity-100" : "scale-0 opacity-0",
+              )}
+            />
+          </div>
+
+          {children}
         </Button>
       </TooltipTrigger>
       <TooltipContent>
