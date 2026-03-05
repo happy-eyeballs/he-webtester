fbea6059988df2841078c0f2db0735c54389f194
Matthias Kirstein <kirm@net.in.tum.de>
2026-03-05 12:44:14 +0100

    UI improvements for mobile

--- setup/webtester/src/components/settings/settings-item.tsx
+++ setup/webtester/src/components/settings/settings-item.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { CircleHelpIcon } from "lucide-react";
 import {
   Tooltip,
@@ -10,25 +10,35 @@ export const SettingsItem: React.FC<{
   label: string;
   helpText?: React.ReactNode;
   children?: React.ReactNode;
-}> = ({ label, helpText, children }) => (
-  <div className="grid md:grid-cols-[230px_1fr] md:gap-x-10 gap-y-2 items-center min-h-8">
-    <div className="text-sm leading-snug">
-      <div className="inline mr-2">{label}</div>
+}> = ({ label, helpText, children }) => {
+  const [isTooltipOpen, setTooltipOpen] = useState(false);
 
-      {helpText && (
-        <Tooltip>
-          <TooltipTrigger asChild={true}>
-            <CircleHelpIcon className="size-4.5 inline align-text-bottom text-blue-400" />
-          </TooltipTrigger>
-          <TooltipContent className="text-sm max-w-200">
-            {helpText}
-          </TooltipContent>
-        </Tooltip>
-      )}
+  return (
+    <div className="grid md:grid-cols-[230px_1fr] md:gap-x-10 gap-y-2 items-center min-h-8">
+      <div className="text-sm leading-snug">
+        <div className="inline mr-2">{label}</div>
+
+        {helpText && (
+          <Tooltip open={isTooltipOpen} onOpenChange={setTooltipOpen}>
+            <TooltipTrigger asChild={true}>
+              <CircleHelpIcon
+                className="size-4.5 inline align-text-bottom text-blue-400"
+                onClick={(event) => {
+                  event.preventDefault();
+                  setTooltipOpen((open) => !open);
+                }}
+              />
+            </TooltipTrigger>
+            <TooltipContent className="text-sm md:max-w-3xl">
+              {helpText}
+            </TooltipContent>
+          </Tooltip>
+        )}
+      </div>
+      <div className="min-w-0">{children}</div>
     </div>
-    <div className="min-w-0">{children}</div>
-  </div>
-);
+  );
+};
 
 export const SettingsDivider: React.FC = () => (
   <div className="border-t my-4" />
